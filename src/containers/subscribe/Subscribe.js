73b05af43a6abe3121483557ccf2d12998f6edb7
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-03-18 19:26:45 +0000

    Add and style Subcription section

--- src/containers/subscribe/Subscribe.js
+++ src/containers/subscribe/Subscribe.js
@@ -4,7 +4,18 @@ import './subscribe.css';
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <div className='eager-escapes__subscribe'>
+      <h1>Find out How we Work!!</h1>
+      <p>Access our "How we work" guide + subscribe!</p>
+      <div className='eager-escapes__subscribe-form'>
+        <form action="">
+          <input type="text" placeholder='Firstname'/>
+          <input type="text" placeholder='Lastname'/>
+          <input type="email" placeholder='Email Address'/>
+          <button type='submit'>Subscribe</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
